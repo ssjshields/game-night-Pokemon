bec4d98203186ad0910b3323aa8af8ea37b611c8
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-05-25 18:18:40 -0400

    distro changes + coin fixes

--- Contents/mods/game-night-Pokemon/media/lua/client/gameNight - implementPokemon.lua
+++ Contents/mods/game-night-Pokemon/media/lua/client/gameNight - implementPokemon.lua
@@ -22,7 +22,7 @@ for typeID, coins in pairs(Pokemon.Tokens.types) do
         table.insert(Pokemon.Tokens.typesToRegister, "Base."..coin)
         gamePieceAndBoardHandler.registerSpecial("Base."..coin, {
             alternateStackRendering = {func="DrawTextureRoundFace", rgb = {0.55, 0.44, 0.33}}, addTextureDir = "PokeTokens/",
-            actions = { coinFlip=true, examine=true }, textureSize = {64,64}, altState=typeID, shiftAction = "coinFlip",
+            actions = { flipPiece=true, coinFlip=true, examine=true }, textureSize = {64,64}, altState=typeID, shiftAction = "coinFlip",
         })
     end
 end
@@ -33,12 +33,12 @@ gamePieceAndBoardHandler.registerSpecial("Base.pokemonStarterKit", {nonGamePiece
 
 gamePieceAndBoardHandler.registerSpecial("Base.PokemonStatusCoin", {
     alternateStackRendering = {func="DrawTextureRoundFace", rgb = {0.55, 0.44, 0.33}}, addTextureDir = "PokeTokens/",
-    actions = { coinFlip=true, examine=true }, textureSize = {64,64}, altState="PokemonStatusCoinFlipped", shiftAction = "coinFlip",
+    actions = { flipPiece=true, coinFlip=true, examine=true }, textureSize = {64,64}, altState="PokemonStatusCoinFlipped", shiftAction = "coinFlip",
 })
 
 gamePieceAndBoardHandler.registerSpecial("Base.MetalPikachuCoin", {
     alternateStackRendering = {func="DrawTextureRoundFace", rgb = {0.55, 0.44, 0.33}}, addTextureDir = "PokeTokens/",
-    actions = { coinFlip=true, examine=true }, textureSize = {64,64}, altState="MetalPikachuCoinFlipped", shiftAction = "coinFlip",
+    actions = { flipPiece=true, coinFlip=true, examine=true }, textureSize = {64,64}, altState="MetalPikachuCoinFlipped", shiftAction = "coinFlip",
 })
 
 
--- Contents/mods/game-night-Pokemon/media/lua/server/gameNight - DistributionsPokemon.lua
+++ Contents/mods/game-night-Pokemon/media/lua/server/gameNight - DistributionsPokemon.lua
@@ -3,38 +3,48 @@ require "Items/SuburbsDistributions"
 local gameNightDistro = require "gameNight - Distributions"
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["pokemonStarterKit"] = {
+    rolls = 6,
     perDistFactor = {
+        ["Gifts"] = 1.5,
+        ["GigamartToys"] = 1.5,
+        ["HolidayStuff"] = 1.5,
+
         ["ClassroomDesk"] = 0.01,
         ["BedroomDresser"] = 0.01,
         ["ClassroomMisc"] = 0.01,
         ["SchoolLockers"] = 0.01,
-        ["OfficeDeskHome"]=0,
-        ["BarCounterMisc"]=0,
-        ["Hobbies"]=0.01,
-        ["WardrobeChild"]=0.01,
-        ["CrateRandomJunk"]=0.01,
+        ["OfficeDeskHome"] = 0,
+        ["BarCounterMisc"] = 0,
+        ["Hobbies"] = 0.01,
+        ["WardrobeChild"] = 0.01,
+        ["CrateRandomJunk"] = 0.01,
     }
 }
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["pokemonBoosterPack"] = {
+    rolls = 12,
     perDistFactor = {
+        ["Gifts"] = 1.5,
+        ["GigamartToys"] = 1.5,
+        ["HolidayStuff"] = 1.5,
+
         ["ClassroomDesk"] = 0.015,
         ["BedroomDresser"] = 0.01,
         ["ClassroomMisc"] = 0.01,
         ["SchoolLockers"] = 0.015,
-        ["OfficeDeskHome"]=0,
-        ["BarCounterMisc"]=0,
-        ["Hobbies"]=0.01,
-        ["WardrobeChild"]=0.01,
-        ["CrateRandomJunk"]=0.01,
+        ["OfficeDeskHome"] = 0,
+        ["BarCounterMisc"] = 0,
+        ["Hobbies"] = 0.01,
+        ["WardrobeChild"] = 0.01,
+        ["CrateRandomJunk"] = 0.01,
     }
 }
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["pokemonCards"] = {
     perDistFactor = {
-        ["Gifts"]=0,
-        ["GigamartToys"]=0,
-        ["HolidayStuff"]=0,
+        ["Gifts"] = 0,
+        ["GigamartToys"] = 0,
+        ["HolidayStuff"] = 0,
         ["SchoolLockers"] = 1.5,
         ["ClassroomDesk"] = 1.5,
     }
